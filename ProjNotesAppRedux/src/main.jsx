f944ed1edb9f0e3cee8828b64212bbaf00f28343
l1f21bsse0147 <farazahmad9887@gmail.com>
2025-02-24 19:54:46 +0500

    fix toaster

--- ProjNotesAppRedux/src/main.jsx
+++ ProjNotesAppRedux/src/main.jsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <App />
-      <Toaster />
+      <Toaster  position="top-right" />
     </Provider>
   </StrictMode>,
 )
